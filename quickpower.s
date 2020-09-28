28ee697e13dd12b936a77e83ee8eaaf0f49d7757
Shauming <50039782+shauming1020@users.noreply.github.com>
2020-09-28 21:10:42 +0800

    Update quickpower.s

--- quickpower.s
+++ quickpower.s
@@ -1,6 +1,6 @@
 .data
-x: .word 2
-y: .word 10 # unsigned
+x: .word 3
+y: .word 5				# unsigned
 str1: .string " ^ "
 str2: .string " = "
 
@@ -8,12 +8,11 @@ str2: .string " = "
 main:
 	lw s0, x			# Set s0 equal to the parameter x
 	lw s1, y			# Set s1 equal to the parameter y
-	mv a0, s0
+	mv a0, s0			# Set arguments
 	mv a1, s1
 	jal power			# return the result a0
 	mv s2, a0			# Set s2 equal to the result
 	j print
-	
 power:
 	addi sp, sp, -12
 	sw ra, 0(sp)
@@ -26,21 +25,21 @@ loop:
 	bge x0, s1, Ret		# if (0 >= y) break
 	andi t0, s1, 0x1	# t0 -> y & 0x1
 	li t1, 0x1			# t1 -> 0x1
-	li ra, 0x54			# Set ra equal to slli s1, s1, 1
+	li ra, 0x54			# Set ra equal to srli s1, s1, 1
 	beq t0, t1, odd		# y is odd
 	srli s1, s1, 1		# y_u >> 1, y /= 2
 	mul s0, s0, s0		# x = x * x
 	j loop				# goto loop
 odd:
 	mul s2, s2, s0		# res = res * x
-	jr ra				# goto slli s1, s1, 1
+	jr ra				# goto srli s1, s1, 1
 Ret:
 	mv a0, s2			# Set return reg a0 
-	lw ra, 0(sp)		
-	lw s0, 4(sp)
-	lw s1, 8(sp)
-	addi sp, sp, 12		
-	jr ra
+	lw ra, 0(sp)		# Restore ra
+	lw s0, 4(sp)		# Restore s0
+	lw s1, 8(sp)		# Restore s1
+	addi sp, sp, 12		# Free space on the stack for the 3 words
+	jr ra				# Return to the caller
 print:
 	mv t0, a0			# Set tmp equal to a0
 	mv a0, s0			# prepare to print x
